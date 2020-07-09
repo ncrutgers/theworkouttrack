8dc736c21fefac3d09a103ded92e3ed12dba368b
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-07-09 03:01:37 -0400

    Created server connection and mongoose

--- server.js
+++ server.js
@@ -19,8 +19,8 @@ mongoose.connect("mongodb://localhost/workout", {
 });
 
 // routes
-//app.use(require("./routes/apiRoutes.js"));
-//app.use(require("./routes/htmlRoutes.js"));
+require("./routes/apiRoutes.js")(app);
+require("./routes/htmlRoutes.js")(app);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
