37fbcca9f0935b1c1eef3c9b68110b98c8dbd0b0
Willow <61257976+ncrutgers@users.noreply.github.com>
2020-07-09 03:30:09 -0400

    Changed db connection

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/workout", {
+// If deployed, use the deployed database.Otherwise use the local workout database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/workout";
+
+// Connect to the MongoDB
+mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false
 });
